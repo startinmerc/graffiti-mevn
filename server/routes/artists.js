411511c9cd52fd3dfb85f160dad81015bf47a947
startinmerc <startinmerc@gmail.com>
2020-11-15 21:21:10 +0000

    Merge from Server

--- server/routes/artists.js
+++ server/routes/artists.js
@@ -5,10 +5,12 @@ const {
 	createArtist,
 	getArtist,
 	getAllArtists,
+	deleteArtistBasic,
 } = require("../handlers/artists");
 
 router.route("/").post(createArtist);
 router.route("/:query").get(getArtist);
+router.route("/:id").delete(deleteArtistBasic);
 router.route("/").get(getAllArtists);
 
 module.exports = router;
