827922502a221744f591ab223c5c9e12d16b3305
startinmerc <startinmerc@gmail.com>
2020-11-17 10:26:48 +0000

    Add ArtistList view

---
+++ client/src/views/ArtistList.vue
@@ -0,0 +1,28 @@
+<template>
+	<main>
+		<h1>Artists</h1>
+		<ul>
+			<li v-for="artist in artists" :key="artist._id">
+				<router-link :to="`/artist/${artist._id}`">{{ artist.name }}</router-link>
+			</li>
+		</ul>
+	</main>
+</template>
+
+<script>
+import { getAllArtists } from "../utils/api";
+
+export default {
+	name: "ArtistList",
+	async mounted() {
+    this.artists = await getAllArtists();
+  },
+  data: function(){
+    return {
+      artists: {}
+    }
+  }
+};
+</script>
+
+<style></style>
