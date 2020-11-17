7977cce352078926895e1d9282d9ad4a1382ab56
startinmerc <startinmerc@gmail.com>
2020-11-17 12:02:55 +0000

    Add methods to ArtistList to get random photo for list item background

--- client/src/views/ArtistList.vue
+++ client/src/views/ArtistList.vue
@@ -15,13 +15,31 @@ import { getAllArtists } from "../utils/api";
 export default {
 	name: "ArtistList",
 	async mounted() {
-    this.artists = await getAllArtists();
-  },
-  data: function(){
-    return {
-      artists: {}
-    }
-  }
+		this.artists = await getAllArtists();
+	},
+	data: function() {
+		return {
+			artists: {},
+		};
+	},
+	methods: {
+		// Gets random photo from random artwork in supplied array
+		getRandomPhoto(artworks) {
+			let artwork =
+				artworks[this.getRandomIntInclusive(0, artworks.length - 1)];
+			let url =
+				artwork.photos[
+					this.getRandomIntInclusive(0, artwork.photos.length - 1)
+				];
+			return url;
+		},
+		// MATHS
+		getRandomIntInclusive(min, max) {
+			min = Math.ceil(min);
+			max = Math.floor(max);
+			return Math.floor(Math.random() * (max - min + 1) + min);
+		},
+	},
 };
 </script>
 
