a7bd5059cd9a9d13d7a7bf7d67a85025d7ec94a0
startinmerc <startinmerc@gmail.com>
2020-11-19 15:48:46 +0000

    Add SubmitArtwork to router

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -6,6 +6,7 @@ import FullMap from "../views/FullMap.vue";
 import ArtworkDetail from "../views/ArtworkDetail.vue";
 import ArtistDetail from "../views/ArtistDetail.vue";
 import ArtistList from "../views/ArtistList.vue";
+import SubmitArtwork from "../views/SubmitArtwork.vue";
 
 Vue.use(VueRouter);
 
@@ -34,6 +35,11 @@ const routes = [
 		path: "/map/:artistID",
 		name: "ArtistOnMap",
 		component: FullMap,
+	},
+	{
+		path: "/artwork/new",
+		name: "SubmitArtwork",
+		component: SubmitArtwork,
 	},
 	{
 		path: "/artwork/:artworkID",
