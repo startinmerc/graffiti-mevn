972af13d3adae23ef8ebabfbbad7791d05fce823
startinmerc <startinmerc@gmail.com>
2020-11-25 09:29:39 +0000

    Use schemas in artwork routes

--- server/routes/artworks.js
+++ server/routes/artworks.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router({ mergeParams: true });
 const multer = require("multer");
 const { storage } = require("../cloudinary");
-const upload = multer({storage});
+const upload = multer({ storage });
+const { artworkPostSchema, artworkPutSchema } = require("../joiSchemas");
 
 const {
 	getArtwork,
@@ -12,9 +13,33 @@ const {
 	deleteArtwork,
 } = require("../handlers/artworks");
 
-router.route("/").post(upload.array("photo"), createArtwork);
+const validatePostArtwork = (req, res, next) => {
+	const { error } = artworkPostSchema.validate(req.body);
+	if (error) {
+		const msg = error.details.map((i) => i.message).join(",");
+		throw new Error(msg);
+	} else {
+		next();
+	}
+};
+
+const validatePutArtwork = (req, res, next) => {
+	const { error } = artworkPutSchema.validate(req.body);
+	if (error) {
+		const msg = error.details.map((i) => i.message).join(",");
+		throw new Error(msg);
+	} else {
+		next();
+	}
+};
+
+router
+	.route("/")
+	.post(validatePostArtwork, upload.array("photo"), createArtwork);
 router.route("/:query").get(getArtwork);
-router.route("/:query").put(editArtwork);
+router
+	.route("/:query")
+	.put(validatePutArtwork, upload.array("photo"), editArtwork);
 router.route("/:query").delete(deleteArtwork);
 router.route("/").get(getAllArtworks);
 
